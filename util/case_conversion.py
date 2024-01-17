ae151bc559b40bc811fcd946ae776a653de56d7c
naglit <docono.ne.ip@gmail.com>
2024-01-17 00:04:52 -0800

    add a case conversion func

--- util/case_conversion.py
+++ util/case_conversion.py
@@ -2,3 +2,6 @@ import re
 
 def to_lower_snake_from_pascal(name: str) -> str:
     return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+
+def to_pascal_from_snake(snake_str: str) -> str:
+    return ''.join(word.capitalize() for word in snake_str.split('_'))
