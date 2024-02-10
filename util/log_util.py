3a107eaec906c74c8e2cca171ba6335aa027bfde
nitoga <nitoga@us.seeknetusa.com>
2024-02-10 15:41:50 -0800

    enable variable name print

--- util/log_util.py
+++ util/log_util.py
@@ -1,6 +1,26 @@
 def print_with_newline(message):
-    info(message)
+    __info(message)
     print("")
 
-def info(message: str):
-    print(message)
+def write_log(message, var_name="") -> None:
+    __debug(
+        f"{var_name}={message}" if len(var_name) > 0\
+        else message)
+
+def __debug(message):
+    __write(message, "debug")
+
+def __info(message):
+    __write(message, "info")
+
+def __warn(message):
+    __write(message, "warn")
+
+def __error(message):
+    __write(message, "error")
+
+def __fatal(message):
+    __write(message, "fatal")
+
+def __write(message, logtype: str):
+    print(f"[{logtype}]{message}")
