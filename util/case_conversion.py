e1a7eca79f13b8e7ff42db045a95634a419bc4d6
nitoga <nitoga@us.seeknetusa.com>
2024-02-11 03:14:41 -0800

    fixed some bugs

--- util/case_conversion.py
+++ util/case_conversion.py
@@ -5,3 +5,6 @@ def to_lower_snake_from_pascal(name: str) -> str:
 
 def to_pascal_from_snake(snake_str: str) -> str:
     return ''.join(word.capitalize() for word in snake_str.split('_'))
+
+def capitalize_first_char(input_string: str) -> str:
+    return input_string.title()
--- util/file_update_repository.py
+++ util/file_update_repository.py
@@ -1,23 +1,23 @@
 import sys, os
 from os import listdir
 from os.path import isfile, join
+import pyutil.util.log_util as log
 
 def write_file(file_name: str, lines: list[str], charset="utf-8"):
     """ write lines into a file """
     file_path = file_name
     data = "".join(lines)
     if "-d" in sys.argv:
-        print("\nDEBUG MODE:")
-        print(data)
+        log.write_log("\nDEBUG MODE:")
+        log.write_log(data)
         return
 
     with open(file_path, "w", encoding=charset) as fout:
-        print(f"Create {file_path}")        
         fout.write(data)
 
 def make_dir(dir_path):
     if "-d" not in sys.argv:
         if not os.path.isdir(dir_path): os.makedirs(dir_path)
     else:
-        print("\nDEBUG MODE:")
-        print(dir_path)
+        log.write_log("\nDEBUG MODE:")
+        log.write_log(dir_path)
